bb1caef9a3cd602faf9dd7c8261a3e68618cc3fc
magicCicero <cicerooantonio20@gmail.com>
2023-05-24 22:26:31 +0200

    add final styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,15 +5,22 @@ let intervalSec = 0;
 let intervalRestart = 0;
 let counterStarted = false;
 const addZero = "0";
+const htmlContainer = document.querySelector("#startbtn");
+const divContainer = document.querySelector("#divcontainer");
 
 const startMinCountdown = () => {
   // Bedingung checkt, ob ein Countdown schon im Gange ist. Falls ja, wird die Funktion nicht mehr ausgeführt und eine "Überlappung" der Funktion wird verhindert
   if (counterStarted) {
     return;
   }
-
   //   Funktion wird normal ausgeführt. Der Input als Zahl wird gespeichert
   let minutesInput = Number(document.querySelector("#minutes").value);
+  // Funktion wird anhand der Eingabe geprüft. Wird eine 0 oder keine Zahl oder eine Zahl kleiner als 0 im Inputfield eingegeben, wird die Funktion ebenfalls nicht ausgeführt
+  if (minutesInput <= 0) {
+    window.alert("Bitte gebe eine Zahl ein");
+    return;
+  }
+
   //   Der Countdown wird in einem Intervall von 1 Sek gestartet
   intervalSec = setInterval(updateCounterSec, 1000);
   //   Gleichzeitig wird der eingegebene Minutenzahl 1 abgezogen und in einer gesonderten Variable gespeichert
@@ -38,7 +45,22 @@ const updateCounterSec = () => {
     seconds = 60;
   } else {
   }
-  //   Ansonsten wird die Sekunden runtergezählt und die OutputFunktion wieder gefeuert.
+  // Totale Sekundenzahl wird hier mit der eingegebenen minuten mal 60 genommen.
+  const totalSeconds = minuteCapture * 60 + seconds;
+  const maxMinutes = Number(document.querySelector("#minutes").value); // Eingegebene Minutenzahl als maximale Minutenzahl verwenden
+  const maxSeconds = maxMinutes * 60;
+  // Berechnung des Hue-Werts für die Setzung der Hintergrundfarbe in Abhängigkeit der eingegebene Zahl. Ist die Minutenzahl relativ klein, dann wird sich der Farbverlauf schneller ändern. Ist die Minutenzahl relativ hoch, dann wird sich der Farbverlauf langsamer verändern
+  const hue = Math.floor((totalSeconds / maxSeconds) * 60);
+  const position = Math.floor((totalSeconds / maxSeconds) * 100) * -1;
+  // Erzeugung der Farbe im HSL-Format für die Änderung im CSS durch JS
+  const rgbColor = `hsl(${hue}, 100%, 50%)`;
+  const divPosition = `${position}%`;
+
+  htmlContainer.style.background = rgbColor;
+  divContainer.style.left = divPosition;
+  divContainer.style.background = rgbColor;
+  divContainer.style.display = "block";
+  //   Ansonsten werden die Sekunden runtergezählt und die OutputFunktion wieder gefeuert.
   seconds--;
   outputData();
 };
@@ -72,3 +94,19 @@ const outputData = () => {
   }
   timeOutput.innerHTML = `${formattedMinute}:${formattedSecond}`;
 };
+// Für den animierten Div Container werden hier Seifenblasen erstellt
+// Anzahl der Seifenblasen
+let bubbleCount = 50;
+
+for (let i = 0; i < bubbleCount; i++) {
+  let bubble = document.createElement("div");
+  bubble.className = "bubble";
+  // Zufällige Verzögerung für jede Seifenblase
+  bubble.style.animationDelay = Math.random() * 6 + "s";
+  // Zufällige horizontale Position
+  bubble.style.left = Math.random() * 100 + "%";
+  // Zufällige vertikale Position
+  bubble.style.top = Math.random() * 100 + "%";
+  // Nach festgelegter Menge werden diese nun im HTML hinzugefügt
+  divContainer.appendChild(bubble);
+}
